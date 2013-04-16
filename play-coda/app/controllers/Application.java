4e3ec912394ad29ffedbe1105e4e6d41938baa49
Martin Grotzke <martin.grotzke@googlemail.com>
2013-04-16 11:50:46 +0200

    Remove check of db thread pool size

--- play-coda/app/controllers/Application.java
+++ play-coda/app/controllers/Application.java
@@ -22,7 +22,6 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
 import scala.concurrent.ExecutionContext;
-import utils.Predicated;
 import akka.dispatch.ExecutionContexts;
 import akka.dispatch.Futures;
 
@@ -45,18 +44,6 @@ public class Application extends Controller {
     private static final ThreadPoolExecutor tpe = new ThreadPoolExecutor(minConnections, maxConnections, 0L,
             TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(), new NamedThreadFactory("dbEc"));
     private static final ExecutionContext dbEc = ExecutionContexts.fromExecutorService(tpe);
-
-    // A predicate for checking our ability to service database requests is determined by ensuring that the request
-    // queue doesn't fill up beyond a certain threshold. For convenience we use the max number of connections
-    // to determine this threshold. It is a rough check as we don't know how many queries we're going
-    // to make or what other threads are running in parallel etc. Nevertheless, the check is adequate in order to
-    // throttle the acceptance of requests to the size of the pool.
-    public static class IsDbAvailable implements Callable<Boolean> {
-        @Override
-        public Boolean call() {
-            return (tpe.getQueue().size() < maxConnections);
-        }
-    }
 
     public static Result index() {
         return ok(views.html.index.render("Your new application is ready."));
@@ -86,7 +73,6 @@ public class Application extends Controller {
     /**
      * Display the list of computers.
      */
-    @Predicated(predicate = IsDbAvailable.class, failed = SERVICE_UNAVAILABLE)
     public static Result listAsync2(final int page, final String sortBy, final String order, final String filter) {
         Promise<Page<Computer>> promise = Akka.asPromise(Futures
                 .future(listCallable(page, sortBy, order, filter), dbEc));
