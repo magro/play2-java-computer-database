a9347ed95e456513395d1b0c5f7063273d4a6481
Martin Grotzke <martin.grotzke@googlemail.com>
2013-04-17 17:25:06 +0200

    Cleanup async controller

--- play-coda/app/controllers/AsyncApplication.java
+++ play-coda/app/controllers/AsyncApplication.java
@@ -1,131 +1,58 @@
 package controllers;
 
-import static utils.JsonUtil.MAPPER;
-
-import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.Callable;
 
-import javax.annotation.Nullable;
-
 import models.Computer;
-import models.PurchasableComputer;
-
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-
 import play.libs.Akka;
 import play.libs.F.Function;
 import play.libs.F.Promise;
 import play.libs.WS;
 import play.libs.WS.Response;
-import play.libs.WS.WSRequestHolder;
 import play.mvc.Controller;
 import play.mvc.Result;
-import utils.PageImpl;
 
 import com.avaje.ebean.Page;
-import com.google.common.collect.Lists;
 
 public class AsyncApplication extends Controller {
 
-    // ====================================== Sync stuff ==========================================
+    // ====================================== Sync/Async slow list ====================================
 
     /**
      * Display the list of computers.
+     * 
+     * @throws InterruptedException
      */
-    public static Result list(final int page, final String sortBy, final String order, final String filter) {
+    // $ ab -c 200 -n 2000 http://localhost:9000/slowList
+    // Requests per second: 149.36 [#/sec] (mean)
+    // Time per request: 1339.015 [ms] (mean)
+    public static Result slowList(final int page, final String sortBy, final String order, final String filter)
+            throws InterruptedException {
         final Page<Computer> items = Computer.page(page, 10, sortBy, order, filter);
-        List<PurchasableComputer> purchasables = getPurchasableComputers(items);
-        final Page<Computer> resultPage = PageImpl.of(purchasables, items);
-        return ok(views.html.list.render(resultPage, sortBy, order, filter));
+        // Ouch, slow backend
+        Thread.sleep(50);
+        return ok(views.html.list.render(items, sortBy, order, filter));
     }
 
-    private static List<PurchasableComputer> getPurchasableComputers(final Page<Computer> items) {
-        return Lists.transform(items.getList(), new com.google.common.base.Function<Computer, PurchasableComputer>() {
+    // $ ab -c 200 -n 2000 http://localhost:9000/asyncSlowList
+    // Requests per second: 158.50 [#/sec] (mean)
+    // Time per request: 1261.863 [ms] (mean)
+    public static Result asyncSlowList(final int page, final String sortBy, final String order, final String filter)
+            throws InterruptedException {
+        Promise<Page<Computer>> promise = Akka.future(new Callable<Page<Computer>>() {
             @Override
-            @Nullable
-            public PurchasableComputer apply(@Nullable final Computer computer) {
-                try {
-                    return getOrderInfo(computer);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
+            public Page<Computer> call() throws Exception {
+                // Ouch, slow backend
+                Thread.sleep(50);
+                return Computer.page(page, 10, sortBy, order, filter);
             }
         });
-    }
-
-    private static PurchasableComputer getOrderInfo(final Computer computer) throws JsonParseException,
-            JsonMappingException, IOException {
-        final Response response = orderInfoWs(computer).get().get();
-        final JsonNode json = MAPPER.readValue(response.getBodyAsStream(), JsonNode.class);
-        int price = json.get("price").getIntValue();
-        int availability = json.get("availability").getIntValue();
-        return PurchasableComputer.create(computer, price, availability);
-    }
-
-    private static WSRequestHolder orderInfoWs(final Computer computer) {
-        return WS.url("http://localhost:9001/orderinfo").setQueryParameter("name", computer.name)
-                .setQueryParameter("company", computer.getCompanyName());
-    }
-
-    // ====================================== Async stuff ==========================================
-
-    /**
-     * Display the list of computers.
-     */
-    public static Result listAsync(final int page, final String sortBy, final String order, final String filter) {
-        Promise<Page<Computer>> items = Akka.future(listCallable(page, sortBy, order, filter));
-        Promise<Page<Computer>> purchasable = items.flatMap(getPurchasableComputers);
-        return async(purchasable.map(new Function<Page<Computer>, Result>() {
+        return async(promise.map(new Function<Page<Computer>, Result>() {
             @Override
-            public Result apply(Page<Computer> items) throws Throwable {
-                return ok(views.html.list.render(items, sortBy, order, filter));
+            public Result apply(Page<Computer> a) throws Throwable {
+                return ok(views.html.list.render(a, sortBy, order, filter));
             }
         }));
     }
-
-    private static Callable<Page<Computer>> listCallable(final int page, final String sortBy, final String order,
-            final String filter) {
-        return new Callable<Page<Computer>>() {
-            @Override
-            public Page<Computer> call() throws Exception {
-                return Computer.page(page, 10, sortBy, order, filter);
-            }
-        };
-    }
-
-    private static Function<Page<Computer>, Promise<Page<Computer>>> getPurchasableComputers = new Function<Page<Computer>, Promise<Page<Computer>>>() {
-        @Override
-        public Promise<Page<Computer>> apply(final Page<Computer> items) throws Throwable {
-            final List<Promise<? extends PurchasableComputer>> purchasablePromises = Lists.transform(items.getList(),
-                    getOrderInfoAsync);
-            final Promise<List<PurchasableComputer>> purchasablesPromise = Promise.sequence(purchasablePromises);
-            return purchasablesPromise.map(new Function<List<PurchasableComputer>, Page<Computer>>() {
-                @Override
-                public Page<Computer> apply(List<PurchasableComputer> a) throws Throwable {
-                    return PageImpl.of(a, items);
-                }
-            });
-        }
-    };
-
-    private static final com.google.common.base.Function<Computer, Promise<? extends PurchasableComputer>> getOrderInfoAsync = new com.google.common.base.Function<Computer, Promise<? extends PurchasableComputer>>() {
-        @Override
-        @Nullable
-        public Promise<? extends PurchasableComputer> apply(final @Nullable Computer computer) {
-            return orderInfoWs(computer).get().map(new Function<WS.Response, PurchasableComputer>() {
-                @Override
-                public PurchasableComputer apply(Response a) throws Throwable {
-                    final JsonNode json = MAPPER.readValue(a.getBodyAsStream(), JsonNode.class);
-                    int price = json.get("price").getIntValue();
-                    int availability = json.get("availability").getIntValue();
-                    return PurchasableComputer.create(computer, price, availability);
-                }
-            });
-        }
-    };
 
     // ============================ Pure WebService sync/async test =======================
 
