b459ec3ae06180a37c57bdd44b05d52a54153f2d
Martin Grotzke <martin.grotzke@googlemail.com>
2013-04-17 16:07:50 +0200

    Retain sort order on live search

--- play-coda/public/javascripts/live_search.js
+++ play-coda/public/javascripts/live_search.js
@@ -2,16 +2,12 @@ window.coda =  window.coda || {};
 
 window.coda.AjaxInput = (function () {
   "use strict";
-
-  // TODO remove in production. Only necessary to prevent
-  // Chrome from optimizing away uncalled functions and properties
-  // from the execution context, making live editing and debugging
-  // difficult
-  eval();
   
   var liveSearch = function liveSearch(event) {
       var q = $("#searchbox").val();
-      jsRoutes.controllers.Application.liveSearch(null, null, null, q).ajax({
+      var sortBy = $(".computers").attr("data-sort-by");
+      var sortOrder = $(".computers").attr("data-sort-order");
+      jsRoutes.controllers.Application.liveSearch(null, sortBy, sortOrder, q).ajax({
         success: function(data, textStatus, jqXHR) {
         	$(document).off('keyup', '#searchbox');
         	window.ajaxUtils.updateHtmlBySelector(data);
